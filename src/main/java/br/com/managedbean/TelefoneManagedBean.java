4c1f023f88c3c1eea525f8229892639fa199dd40
Ssxund3r <gabrielfillip@gmail.com>
2022-07-20 19:32:16 -0300

    Passando parâmetros para outras telas

---
+++ src/main/java/br/com/managedbean/TelefoneManagedBean.java
@@ -0,0 +1,36 @@
+package br.com.managedbean;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import br.com.dao.DaoUsuario;
+import br.com.model.UsuarioPessoa;
+
+@ManagedBean(name = "telefoneManagedBean")
+@ViewScoped
+public class TelefoneManagedBean {
+
+	private UsuarioPessoa user = new UsuarioPessoa();
+	
+	private DaoUsuario<UsuarioPessoa> daoUser = new DaoUsuario<UsuarioPessoa>();
+	
+	@PostConstruct
+	public void init() {
+		String coduser = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap()
+				.get("codigouser");
+		
+		user = daoUser.pesquisar(Long.parseLong(coduser), UsuarioPessoa.class);
+		
+	}
+
+	public UsuarioPessoa getUser() {
+		return user;
+	}
+
+	public void setUser(UsuarioPessoa user) {
+		this.user = user;
+	}
+
+}
