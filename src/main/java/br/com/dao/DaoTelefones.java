12868b182e3b7da0a42daa3d7affc0b304532069
Ssxund3r <gabrielfillip@gmail.com>
2022-07-20 09:28:00 -0300

    Desenvolvimento e teste - Classe Telefone

---
+++ src/main/java/br/com/dao/DaoTelefones.java
@@ -0,0 +1,7 @@
+package br.com.dao;
+
+import br.com.model.TelefoneUser;
+
+public class DaoTelefones  extends GenericDao<TelefoneUser>{
+	
+}
--- src/test/java/br/com/jsfprimefaceshibernate/TestHibernate.java
+++ src/test/java/br/com/jsfprimefaceshibernate/TestHibernate.java
@@ -3,24 +3,42 @@ package br.com.jsfprimefaceshibernate;
 import org.junit.Test;
 
 import br.com.dao.GenericDao;
+import br.com.model.TelefoneUser;
 import br.com.model.UsuarioPessoa;
 
 public class TestHibernate {
 	@Test
 	public void testeHibernateUtil() {
-		
-		GenericDao<UsuarioPessoa> daoGeneric =
-				new GenericDao<UsuarioPessoa>();// Instância o DAO genérico
+
+		GenericDao<UsuarioPessoa> daoGeneric = new GenericDao<UsuarioPessoa>();// Instância o DAO genérico
 		UsuarioPessoa pessoa = new UsuarioPessoa(); // Cria o objeto para ser salvo
 
 		// Seta todas as propriedades do objeto
-		pessoa.setIdade(45);
+		pessoa.setIdade(999);
 		pessoa.setLogin("teste");
-		pessoa.setNome("Paulo");
+		pessoa.setNome("Laricó");
 		pessoa.setSenha("123");
-		pessoa.setSobreNome("Egidio");
+		pessoa.setSobreNome("DoBanhado");
 
 		daoGeneric.salvar(pessoa);// Chama o salvar para gravar no banco de dados.
 
 	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Test
+	public void gravaTelefone() {
+		GenericDao genericDao = new GenericDao();
+
+		UsuarioPessoa pessoa = (UsuarioPessoa) genericDao.pesquisar(17L, UsuarioPessoa.class);
+
+		TelefoneUser telefoneUser = new TelefoneUser();
+
+		telefoneUser.setTipo("Casa");
+		telefoneUser.setNumero("999999999");
+		telefoneUser.setPessoa(pessoa);
+
+		genericDao.salvar(telefoneUser);
+
+	}
+
 }
